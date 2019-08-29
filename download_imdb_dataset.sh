96b6b8bebc9d3e8712ca842a9a8fd3a7fd36be4e
Ongun Uzay Macar <oum2000@columbia.edu>
2019-08-29 19:53:38 -0400

    Added shebang & directory creating lines.

--- download_imdb_dataset.sh
+++ download_imdb_dataset.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+# Create data directory
+mkdir data
 # Download & extract the zipped dataset from URL
 curl http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz --output aclImdb_v1.tar.gz
 # Unzip the file
